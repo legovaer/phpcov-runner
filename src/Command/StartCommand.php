7f62c5b1909c9c5b41959cd5e25f70338aceb250
Levi Govaerts <levi.govaerts@capgemini.com>
2016-06-06 15:11:49 +0100

    Fixing PHP Strict standars issue

--- src/Command/StartCommand.php
+++ src/Command/StartCommand.php
@@ -4,6 +4,8 @@ namespace Legovaer\PHPCOVRunner\Command;
 
 use Legovaer\PHPCOVRunner\Driver\XdebugSQLite3;
 use Symfony\Component\Console\Command\Command as AbstractCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Command used for starting a code coverage analysis.
@@ -21,7 +23,7 @@ class StartCommand extends AbstractCommand
     /**
      * Executes the current command.
      **/
-    protected function execute()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $driver = new XdebugSQLite3();
         $driver->resetLog();
